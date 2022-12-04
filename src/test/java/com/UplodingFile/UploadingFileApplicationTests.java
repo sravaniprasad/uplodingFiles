e562021138020fba97ae032dc3c04555a6fcf7fd
sravaniprasad <99490552+sravaniprasad@users.noreply.github.com>
2022-12-04 20:31:06 +0530

    Update UploadingFileApplicationTests.java

--- src/test/java/com/UplodingFile/UploadingFileApplicationTests.java
+++ src/test/java/com/UplodingFile/UploadingFileApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UploadingFileApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
 }
